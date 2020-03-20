32962d454060816c61a90df54e96508f8d9fb44b
Alexanderkievit <55195393+Alexanderkievit@users.noreply.github.com>
2020-03-20 11:04:51 +0100

    De route werkt

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-import http.server
-import socketserver
-from flask import Flask, request, jsonify, redirect, render_template
+from flask import Flask, request, jsonify, redirect, render_template, send_from_directory
 from flask_cors import CORS
 
 from nosql_db_connectie import goed
@@ -8,20 +6,30 @@ from nosql_db_connectie import goed
 app = Flask(__name__)
 CORS(app)
 
+# webserver
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.run()
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
-@app.route("/ingevoerd")
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+
+
+# api bezig
+@app.route("/ingevoerd", methods=['GET'])
 def ingevoerd():
     return jsonify(goed), 200, {'ContentType': 'application/json'}
 
-PORT = 8080
 
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("servering at port", PORT)
-    httpd.serve_forever()
+app.run()
