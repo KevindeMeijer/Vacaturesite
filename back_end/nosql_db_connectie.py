ec59c70c5e643749f56f7d4dc0a0e3727dea99af
Alexanderkievit <55195393+Alexanderkievit@users.noreply.github.com>
2020-03-27 10:23:56 +0100

    Werkende Word2Vec
    De Word2Vec werkt en de variabelen hebben goede namen gekregen

--- back_end/nosql_db_connectie.py
+++ back_end/nosql_db_connectie.py
@@ -11,26 +11,25 @@ mydb = myclient[dbname]
 
 # Zien van de connectie
 print(mydb)
-
+#De functie om alle punctuations er uit te halen.
 translator = str.maketrans('', '', string.punctuation)
 
 vacatures = []
 vacature_description = []
-vacature_list = []
+divided_sentense_vacature_list = []
 
-# Krijgen van alle vacatures in de database
-for post in mydb.Vacatures.find():
-    vacatures.append(post)
+# Krijgen van alle volledige vacatures in de database
+for vacature in mydb.Vacatures.find():
+    vacatures.append(vacature)
 
-for beschrijving in mydb.Vacatures.find():
-    sentences = beschrijving['description'].split('.')
+#Het splitten van beschrijvingen van alle vacatures
+for description in mydb.Vacatures.find():
+    sentences = description['description'].split('.')
     for sentence in sentences:
         vacature_description.append(sentence.translate(translator).lower())
 
-for losse_vacature in vacature_description:
-    los = losse_vacature.split()
-    vacature_list.append(los)
-
-pprint.pprint(vacatures)
-# print(vacature_list)
+#Het zeten vna elke sentence in een list
+for divided_sentence in vacature_description:
+    divided = divided_sentence.split()
+    divided_sentense_vacature_list.append(divided)
 
