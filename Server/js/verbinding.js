60262af2604ad44f9ece81c92a5230526e3d68a3
Alexanderkievit <55195393+Alexanderkievit@users.noreply.github.com>
2020-06-09 11:01:19 +0200

    Foutcodes

--- Server/js/verbinding.js
+++ Server/js/verbinding.js
@@ -42,4 +42,36 @@ function report() {
         return response.json();
     });
     return false;
-};
+}
+
+window.onload   = () => console.log('window.onload works');
+
+function load_reports() {
+    let naam_1 = document.getElementById('naam_1');
+    let telefoonnummer_1 = document.getElementById('telefoonnummer_1');
+    let mail_1 = document.getElementById('mail_1');
+    let onderwerp_1 = document.getElementById('onderwerp_1');
+    let bericht_1 = document.getElementById('bericht_1');
+
+window.onload = () => {
+    fetch('http://127.0.0.1:5000/load_reports')
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            result = result.split(") (");
+            result_id_contact = result[0];
+            result_naam = result[1];
+            result_telefoonnummer = result[2];
+            result_mail = result[3];
+            result_onderwerp = result[4];
+            result_bericht = result[5];
+
+            naam_1.innerHTML = result_naam;
+            telefoonnummer_1.innerHTML = result_telefoonnummer;
+            mail_1.innerHTML = result_mail;
+            onderwerp_1.innerHTML = result_onderwerp;
+            bericht_1.innerHTML = result_bericht;
+    })
+    .catch(console.error);
+    }
+}
