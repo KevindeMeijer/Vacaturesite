34addbdb6a98cbb0b92605c63dd045615d3a9eb8
Alexanderkievit <55195393+Alexanderkievit@users.noreply.github.com>
2020-06-16 10:54:32 +0200

    Bijna klaar

--- Server/js/frontend_graph.js
+++ Server/js/frontend_graph.js
@@ -1,28 +1,30 @@
 google.charts.load('current', {
     'packages': ['corechart']
 });
+load_chars_frontend()
+
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Month', 'Trend'],
-        ['January', 11],
-        ['February', 17],
-        ['March', 25],
-        ['April', 24],
-        ['May', 97],
-        ['Juno', 1],
-        ['July', 99],
-        ['August', 14],
-        ['September', 35],
-        ['Oktober', 37],
-        ['November', 41],
-        ['December', 109]
+        ['January', resultarray_frontend[0]],
+        ['February', resultarray_frontend[1]],
+        ['March', resultarray_frontend[2]],
+        ['April', resultarray_frontend[3]],
+        ['May', resultarray_frontend[4]],
+        ['Juno', resultarray_frontend[5]],
+        ['July', resultarray_frontend[6]],
+        ['August', resultarray_frontend[7]],
+        ['September', resultarray_frontend[8]],
+        ['Oktober', resultarray_frontend[9]],
+        ['November', resultarray_frontend[10]],
+        ['December', resultarray_frontend[11]]
     ]);
 
     var options = {
         title: 'Front-End 2016',
-        curveType: 'function',
+        // curveType: 'function',
         legend: {
             position: 'bottom'
         }
--- Server/js/verbinding.js
+++ Server/js/verbinding.js
@@ -77,77 +77,35 @@ function load_reports() {
     })
     .catch(console.error);
 }
-
+resultarray_frontend = []
 function load_chars_frontend() {
     fetch('http://127.0.0.1:5000/load_chars_frontend')
     .then(response => response.json())
     .then(result =>{
-        jan_f = result[0]
-        feb_f = result[1]
-        mrt_f = result[2]
-        apr_f = result[3]
-        mei_f = result[4]
-        jun_f = result[5]
-        jul_f = result[6]
-        aug_f = result[7]
-        sep_f = result[8]
-        okt_f = result[9]
-        nov_f = result[10]
-        dec_f = result[11]
+        resultarray_frontend = result
     })
 }
-
+resultarray_backend = []
 function load_chars_backend() {
     fetch('http://127.0.0.1:5000/load_chars_backend')
     .then(response => response.json())
     .then(result =>{
-        jan_b = result[0]
-        feb_b = result[1]
-        mrt_b = result[2]
-        apr_b = result[3]
-        mei_b = result[4]
-        jun_b = result[5]
-        jul_b = result[6]
-        aug_b = result[7]
-        sep_b = result[8]
-        okt_b = result[9]
-        nov_b = result[10]
-        dec_b = result[11]
+        resultarray_backend = result
     })
 }
+resultarray_productowner = []
 function load_chars_productowner() {
     fetch('http://127.0.0.1:5000/load_chars_productowner')
     .then(response => response.json())
     .then(result =>{
-        jan_p = result[0]
-        feb_p = result[1]
-        mrt_p = result[2]
-        apr_p = result[3]
-        mei_p = result[4]
-        jun_p = result[5]
-        jul_p = result[6]
-        aug_p = result[7]
-        sep_p = result[8]
-        okt_p = result[9]
-        nov_p = result[10]
-        dec_p = result[11]
+        resultarray_productowner = result
     })
 }
+resultarray_cloud_security = []
 function load_chars_cloud_security() {
     fetch('http://127.0.0.1:5000/load_chars_cloud_security')
     .then(response => response.json())
     .then(result =>{
-        jan_c = result[0]
-        feb_c = result[1]
-        mrt_c = result[2]
-        apr_c = result[3]
-        mei_c = result[4]
-        jun_c = result[5]
-        jul_c = result[6]
-        aug_c = result[7]
-        sep_c = result[8]
-        okt_c = result[9]
-        nov_c = result[10]
-        dec_c = result[11]
+        resultarray_cloud_security = result
     })
 }
