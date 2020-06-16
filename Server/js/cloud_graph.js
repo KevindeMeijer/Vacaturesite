c1600f1b21ff9e36e8f7b60a3a77f7cb514c3fc3
Alexanderkievit <55195393+Alexanderkievit@users.noreply.github.com>
2020-06-16 13:54:04 +0200

    Veranderingen aan de grafieken

--- Server/js/cloud_graph.js
+++ Server/js/cloud_graph.js
@@ -8,18 +8,18 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Month', 'Trend'],
-        ['January', resultarray_cloud_security[0]],
-        ['February', resultarray_cloud_security[1]],
-        ['March', resultarray_cloud_security[2]],
-        ['April', resultarray_cloud_security[3]],
-        ['May', resultarray_cloud_security[4]],
-        ['Juno', resultarray_cloud_security[5]],
-        ['July', resultarray_cloud_security[6]],
-        ['August', resultarray_cloud_security[7]],
-        ['September', resultarray_cloud_security[8]],
-        ['Oktober', resultarray_cloud_security[9]],
-        ['November', resultarray_cloud_security[10]],
-        ['December', resultarray_cloud_security[11]]
+        [{v: 1, f: 'January'}, resultarray_cloud_security[0]],
+        [{v: 2, f: 'February'}, resultarray_cloud_security[1]],
+        [{v: 3, f: 'March'}, resultarray_cloud_security[2]],
+        [{v: 4, f: 'April'}, resultarray_cloud_security[3]],
+        [{v: 5, f: 'May'}, resultarray_cloud_security[4]],
+        [{v: 6, f: 'Juno'}, resultarray_cloud_security[5]],
+        [{v: 7, f: 'July'}, resultarray_cloud_security[6]],
+        [{v: 8, f: 'August'}, resultarray_cloud_security[7]],
+        [{v: 9, f: 'September'}, resultarray_cloud_security[8]],
+        [{v: 10, f: 'Oktober'}, resultarray_cloud_security[9]],
+        [{v: 11, f: 'November'}, resultarray_cloud_security[10]],
+        [{v: 12, f: 'December'}, resultarray_cloud_security[11]]
     ]);
 
     var options = {
@@ -27,6 +27,12 @@ function drawChart() {
         // curveType: 'function',
         legend: {
             position: 'bottom'
+        },
+        trendlines: {
+            0: {type: 'linear', color: '#000', opacity: 0.5}
+        },
+        hAxis: {
+            ticks: [{v: 1, f: 'January'}, {v: 2, f: 'February'}, {v: 3, f: 'March'}, {v: 4, f: 'April'}, {v: 5, f: 'May'}, {v: 6, f: 'Juno'}, {v: 7, f: 'July'}, {v: 8, f: 'August'}, {v: 9, f: 'September'}, {v: 10, f: 'Oktober'}, {v: 11, f: 'November'}, {v: 12, f: 'December'}]
         }
     };
 
--- Server/js/frontend_graph.js
+++ Server/js/frontend_graph.js
@@ -8,18 +8,18 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Month', 'Trend'],
-        ['January', resultarray_frontend[0]],
-        ['February', resultarray_frontend[1]],
-        ['March', resultarray_frontend[2]],
-        ['April', resultarray_frontend[3]],
-        ['May', resultarray_frontend[4]],
-        ['Juno', resultarray_frontend[5]],
-        ['July', resultarray_frontend[6]],
-        ['August', resultarray_frontend[7]],
-        ['September', resultarray_frontend[8]],
-        ['Oktober', resultarray_frontend[9]],
-        ['November', resultarray_frontend[10]],
-        ['December', resultarray_frontend[11]]
+        [{v: 1, f: 'January'}, resultarray_frontend[0]],
+        [{v: 2, f: 'February'}, resultarray_frontend[1]],
+        [{v: 3, f: 'March'}, resultarray_frontend[2]],
+        [{v: 4, f: 'April'}, resultarray_frontend[3]],
+        [{v: 5, f: 'May'}, resultarray_frontend[4]],
+        [{v: 6, f: 'Juno'}, resultarray_frontend[5]],
+        [{v: 7, f: 'July'}, resultarray_frontend[6]],
+        [{v: 8, f: 'August'}, resultarray_frontend[7]],
+        [{v: 9, f: 'September'}, resultarray_frontend[8]],
+        [{v: 10, f: 'Oktober'}, resultarray_frontend[9]],
+        [{v: 11, f: 'November'}, resultarray_frontend[10]],
+        [{v: 12, f: 'December'}, resultarray_frontend[11]]
     ]);
 
     var options = {
@@ -27,6 +27,12 @@ function drawChart() {
         // curveType: 'function',
         legend: {
             position: 'bottom'
+        },
+        trendlines: {
+            0: {type: 'linear', color: '#000', opacity: 0.5}
+        },
+        hAxis: {
+            ticks: [{v: 1, f: 'January'}, {v: 2, f: 'February'}, {v: 3, f: 'March'}, {v: 4, f: 'April'}, {v: 5, f: 'May'}, {v: 6, f: 'Juno'}, {v: 7, f: 'July'}, {v: 8, f: 'August'}, {v: 9, f: 'September'}, {v: 10, f: 'Oktober'}, {v: 11, f: 'November'}, {v: 12, f: 'December'}]
         }
     };
 
